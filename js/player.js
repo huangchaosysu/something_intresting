66e4d47c84138c3120f375f30e2b11d4376f97c5
超黄 <chaohuang@chaohuangdeMacBook-Air.local>
2018-05-20 23:02:17 +0800

    解决marker多了之后浏览器卡死问题

--- js/player.js
+++ js/player.js
@@ -80,25 +80,14 @@ class VueApp {
                         });
                 },
                 initmap: function() {
-                    axios.get('/data/places.json')
-                        .then(
-                            function(response){
-                                var points = []
-                                // var beijing = new BMap.Point(39.904211, 116.407395);
-                                // points.push(beijing);
-                                for (var p of response.data){
-                                    points.push(new BMap.Point(p.geo[1], p.geo[0]));
-                                    // var marker = new BMap.Marker(point, {title: p.name});
-                                    // window.baidu_map.addOverlay(marker);
-                                }
-                                var options = {
-                                    size: BMAP_POINT_SIZE_NORMAL,
-                                    shape: BMAP_POINT_SHAPE_STAR,
-                                    color: '#d340c3'
-                                }
-                                var pointCollection = new BMap.PointCollection(points, options);
-                                window.baidu_map.addOverlay(pointCollection);
-                        });
+                    axios.get('/data/places.json').then(function(response){ var markers=[];
+                        for (var i of response.data){
+                            var p = new BMap.Point(i.geo[1], i.geo[0]);
+                            markers.push(new BMap.Marker(p, {title: i.name}));
+                        }
+                        window.markerClusterer = new BMapLib.MarkerClusterer(window.baidu_map, {markers:markers});
+                    });
+                    
                 }
             }
         })
